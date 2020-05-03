04e02bb64a406e2dbc55eb9ccbe2a67122c35082
Zhichao Yin <yzcjtr@gmail.com>
2020-05-02 22:35:00 -0400

    Support SSIM loss.

--- losses.py
+++ losses.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from ssim import ssim
 
 def compute_gradient_mask(img, thresh=0.03):
     img = img.mean(dim=1)
@@ -15,16 +16,21 @@ def compute_gradient_mask(img, thresh=0.03):
     mask = mask.unsqueeze(1).float()
     return mask
 
+def robust_l1(x, q=0.5, eps=1e-2):
+    x = torch.pow((x.pow(2) + eps), q)
+    x = x.mean()
+    return x
+
 class photometric_reconstruction_loss(nn.Module):
     def __init__(self):
         super(photometric_reconstruction_loss, self).__init__()
-    
+
     def forward(self, input_img, output_img, exp_mask=None, pose_vec_weight=None, validate=False):
         B,_,h,w = input_img.size()
-        output_img = nn.functional.adaptive_avg_pool2d(output_img, (h, w))  
+        output_img = nn.functional.adaptive_avg_pool2d(output_img, (h, w))
         reconstruction_loss = 0
         out_of_bound = 1 - (input_img == 0).prod(1, keepdim=True).type_as(input_img)
-        diff = (output_img - input_img).abs() * out_of_bound 
+        diff = (output_img - input_img).abs() * out_of_bound
         if exp_mask is not None and validate == False:
             diff=diff*(exp_mask.expand_as(diff)) #expand_as makes the 1 channel mask 3 channels if imgs are colour
         if pose_vec_weight is not None:
@@ -44,10 +50,44 @@ class photometric_reconstruction_loss(nn.Module):
         return reconstruction_loss
 
 
+class photometric_reconstruction_loss_with_ssim(nn.Module):
+    def __init__(self):
+        super(photometric_reconstruction_loss_with_ssim, self).__init__()
+
+    def forward(self, input_img, output_img, exp_mask=None, pose_vec_weight=None, validate=False):
+        B,_,h,w = input_img.size()
+        output_img = nn.functional.adaptive_avg_pool2d(output_img, (h, w))
+        reconstruction_loss = 0
+        out_of_bound = 1 - (input_img == 0).prod(1, keepdim=True).type_as(input_img)
+        ssim_loss = 1 - ssim(output_img, input_img) * out_of_bound
+        diff = (output_img - input_img) * out_of_bound
+        if exp_mask is not None and validate == False:
+            diff=diff*(exp_mask.expand_as(diff)) #expand_as makes the 1 channel mask 3 channels if imgs are colour
+            ssim_loss = ssim_loss*(exp_mask.expand_as(ssim_loss))
+        if pose_vec_weight is not None:
+            pose_vec_mask = pose_vec_weight[:,3:6].norm(dim=1) >=0.005
+            pose_vec_mask = pose_vec_mask.cpu().detach().numpy().reshape((-1,1))
+            pose_vec_mask = np.repeat(pose_vec_mask, output_img.size(2),axis=1)
+            pose_vec_mask = np.repeat(pose_vec_mask.reshape((-1, output_img.size(2),1)),output_img.size(3),axis=2)
+            pose_vec_mask = torch.FloatTensor(pose_vec_mask).unsqueeze(1)#.expand_as(output_img)
+            if validate == False:
+                diff = diff + 4*diff*(pose_vec_mask.expand_as(diff).type_as(output_img))
+                ssim_loss = ssim_loss + 4*ssim_loss*(pose_vec_mask.expand_as(ssim_loss).type_as(ssim_loss))
+            if validate == True:
+                gradient_mask = compute_gradient_mask(output_img)
+                diff = diff*gradient_mask.expand_as(input_img)
+                diff = diff*(pose_vec_mask.expand_as(diff).type_as(output_img))
+                ssim_loss = ssim_loss*gradient_mask.expand_as(ssim_loss)
+                ssim_loss = ssim_loss*(pose_vec_mask.expand_as(ssim_loss).type_as(ssim_loss))
+        reconstruction_loss += 0.5 * (robust_l1(diff) + 0.5 * ssim_loss.mean())
+        assert((reconstruction_loss == reconstruction_loss).item() == 1)
+        return reconstruction_loss
+
+
 class explainability_loss(torch.nn.Module):
     def __init__(self):
         super(explainability_loss, self).__init__()
-    
+
     def forward(self, mask):
         if type(mask) not in [tuple, list]:
             mask = [mask]
@@ -55,8 +95,8 @@ class explainability_loss(torch.nn.Module):
         for mask_scaled in mask:
             ones_var = torch.ones(1).expand_as(mask_scaled).type_as(mask_scaled)
             loss += nn.functional.binary_cross_entropy(mask_scaled, ones_var)
-        return loss  
-    
+        return loss
+
 class Compute_Loss(nn.modules.Module):
     def __init__(self, spatial_trans, photometric_loss, exp_loss, exp_weight=0.08):
         super(Compute_Loss, self).__init__()
@@ -64,15 +104,25 @@ class Compute_Loss(nn.modules.Module):
         self.photometric_loss = photometric_loss
         self.exp_loss = exp_loss
         self.exp_weight = exp_weight
-        
+
     def forward(self, current_img, target_img, pose, exp_mask, disparities, intrinsics, pose_vec_weight=None, validate=False):
         loss = 0
         depth = [1.0/disp for disp in disparities]
         current_pose = -pose.clone()
-        for d, m in zip(depth,exp_mask):        
+        for d, m in zip(depth,exp_mask):
             x_reconstructed = self.spatial_trans(current_img, d, current_pose, intrinsics, intrinsics.inverse())
-            loss += self.photometric_loss(x_reconstructed, target_img, exp_mask=m, pose_vec_weight=pose_vec_weight, validate=validate) 
-               
+            loss += self.photometric_loss(x_reconstructed, target_img, exp_mask=m, pose_vec_weight=pose_vec_weight, validate=validate)
+
         if exp_mask[0] is not None and validate==False:
             loss += self.exp_weight*self.exp_loss(exp_mask)
         return loss
+
+if __name__ == '__main__':
+    loss = photometric_reconstruction_loss_with_ssim()
+    input_img = torch.rand([3,3,10,10], requires_grad=True)
+    output_img = torch.rand([3,3,10,10], requires_grad=True)
+    exp_mask = torch.rand([3,1,10,10], requires_grad=True)
+    pose_vec_weight = torch.rand([3,6], requires_grad=True)
+    validate = False
+    res = loss(input_img, output_img, exp_mask, pose_vec_weight, validate)
+    print(res)
